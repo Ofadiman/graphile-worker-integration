e71c7a75c8064d2d5f04c7489fec911c6ea7e434
ofadiman <ofadiman@gmail.com>
2022-10-28 21:51:20 +0200

    chore: initialize project

---
+++ apps/api/src/main.ts
@@ -0,0 +1,15 @@
+import { Logger } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+
+import { AppModule } from './app.module'
+
+const PORT = process.env.PORT || 3333
+
+async function bootstrap() {
+  const nestApplication = await NestFactory.create(AppModule)
+  await nestApplication.listen(PORT)
+}
+
+bootstrap().then(() => {
+  Logger.log(`🚀 Application is running on: http://localhost:${PORT}`)
+})
